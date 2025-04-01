6d3035bf58cea8e75cc952c93026bb4538dfb28d
Redgel! <redgelgregory.mefania@cit.edu>
2025-04-01 19:47:08 +0800

    run without a problem
    run without a problem but still havent connected

--- backend/SPRINGBOOT_PROJECT/swiftthrift/swiftthrift/src/main/java/edu/cit/swiftthrift/repository/ProductRatingRepository.java
+++ backend/SPRINGBOOT_PROJECT/swiftthrift/swiftthrift/src/main/java/edu/cit/swiftthrift/repository/ProductRatingRepository.java
@@ -1,12 +1,10 @@
 package edu.cit.swiftthrift.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.cit.swiftthrift.entity.ProductRating;
 
 public interface ProductRatingRepository extends JpaRepository<ProductRating, Integer> {
-    Optional<ProductRating> findByProductRatingID(int productRatingId); 
+   
 }
 
--- backend/SPRINGBOOT_PROJECT/swiftthrift/swiftthrift/src/main/java/edu/cit/swiftthrift/repository/StoreRatingRepository.java
+++ backend/SPRINGBOOT_PROJECT/swiftthrift/swiftthrift/src/main/java/edu/cit/swiftthrift/repository/StoreRatingRepository.java
@@ -1,12 +1,9 @@
 package edu.cit.swiftthrift.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import edu.cit.swiftthrift.entity.StoreRating;
 
 public interface StoreRatingRepository extends JpaRepository<StoreRating, Integer> {
-    Optional<StoreRating> findByProductRatingID(int productRatingId); 
 }
 
