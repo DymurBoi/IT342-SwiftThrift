373feace242001522014cc7af4ad11475eee6024
Redgel! <redgelgregory.mefania@cit.edu>
2025-04-09 08:07:09 +0800

    Added WishlistItem
    Added WishlistItem

--- backend/SPRINGBOOT_PROJECT/swiftthrift/swiftthrift/src/main/java/edu/cit/swiftthrift/entity/Wishlist.java
+++ backend/SPRINGBOOT_PROJECT/swiftthrift/swiftthrift/src/main/java/edu/cit/swiftthrift/entity/Wishlist.java
@@ -1,12 +1,16 @@
 package edu.cit.swiftthrift.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -45,4 +49,16 @@ public class Wishlist {
     public void setUser(User user) {
         this.user = user;
     }
+
+    @OneToMany(mappedBy = "wishlist", cascade = CascadeType.ALL)
+    private List<WishlistItem> wishlistItems = new ArrayList<>();
+
+    public List<WishlistItem> getWishlistItems() {
+        return wishlistItems;
+    }
+
+    public void setWishlistItems(List<WishlistItem> wishlistItems) {
+        this.wishlistItems = wishlistItems;
+    }
+
 }
